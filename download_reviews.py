40e7c9e2f4dc4bd5fd7fe1f8b917590b14b7f841
weavermonkey <vembarvarun@gmail.com>
2018-02-14 17:51:25 +0530

    Add function to save webpage to CSV

--- download_reviews.py
+++ download_reviews.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 import time
 import requests
 from pprint import pprint
+import pandas as pd
 
 
 def choose_ebert_reviews():
@@ -43,18 +44,30 @@ def get_rating(movie_name, star_param):
 
 
 def read_html_page(home_page):
-    movie_review_dict = {}
+    movie_review_dict = {'movie_title': [], 'reviewed_by': [], 'score': [], 'review_url': []}
     result = requests.get(url=home_page)
     result_content = result.content
     soup_obj = BeautifulSoup(result_content, 'html5lib')
     wrapper_class = soup_obj.find('div', id='review-list')
-    for x in wrapper_class.find_all('figure'):
+    for curr_movie_dom in wrapper_class.find_all('figure'):
         star_list = []
-        movie_title = x.find('h5', class_='title').a.get_text()
-        movie_critic = x.find('p', class_='byline').get_text()
-        convoluted_rating = x.find('span', class_='star-rating').find_all('i')
+        movie_title = curr_movie_dom.find('h5', class_='title').a.get_text()
+        movie_critic = curr_movie_dom.find('p', class_='byline').get_text().strip()
+        convoluted_rating = curr_movie_dom.find('span', class_='star-rating').find_all('i')
         movie_review_score = get_rating(movie_title, convoluted_rating)
-        print type(x.find('h5', class_='title').find_all('a'))
+        movie_review_url = home_page + curr_movie_dom.find('h5', class_='title').find_all('a')[0]['href'][8:]
+        movie_review_dict['movie_title'].append(movie_title)
+        movie_review_dict['reviewed_by'].append(movie_critic)
+        movie_review_dict['score'].append(movie_review_score)
+        movie_review_dict['review_url'].append(movie_review_url)
+    return movie_review_dict
 
 
-read_html_page('http://www.rogerebert.com/reviews')
+def save_webpage_to_csv():
+    movie_details = read_html_page('http://www.rogerebert.com/reviews')
+    movie_df = pd.DataFrame.from_dict(movie_details)
+    # print movie_df.head()
+    movie_df.to_csv('roger_ebert_reviews', index=False)
+
+
+save_webpage_to_csv()
