ec91e808acb2d98fb5d4bef71d9cbe2a9d3d1213
weavermonkey <vembarvarun@gmail.com>
2018-02-14 17:24:37 +0530

    Add function to movie name, reviewer, and movie score from loaded page

--- download_reviews.py
+++ download_reviews.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
-
-driver = webdriver.Firefox()
+import requests
+from pprint import pprint
 
 
 def choose_ebert_reviews():
     # driver = webdriver.Firefox()
+    driver = webdriver.Firefox()
     driver.get('http://www.rogerebert.com/reviews')
     driver.find_element_by_xpath(
         '/html/body/div[1]/div/section/form/section/fieldset[3]/div[1]/div/ul').click()
@@ -15,17 +16,45 @@ def choose_ebert_reviews():
 
 
 def scroll_to_bottom(webpage):
+    driver = webdriver.Firefox()
     driver.get(webpage)
-    lenOfPage = driver.execute_script(
-        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    len_of_page = driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);var len_of_page=document.body.scrollHeight;return len_of_page;")
     match = False
     while (match == False):
-        lastCount = lenOfPage
-        time.sleep(3)
-        lenOfPage = driver.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        if lastCount == lenOfPage:
+        last_count = len_of_page
+        time.sleep(0.43)
+        len_of_page = driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);var len_of_page=document.body.scrollHeight;return len_of_page;")
+        if last_count == len_of_page:
             match = True
 
 
-scroll_to_bottom('http://www.rogerebert.com/reviews')
+def get_rating(movie_name, star_param):
+    movie_rating = 0.0
+    star_list = [i.prettify().replace("\n", "").replace("<i class=\"", "").replace("></i>", "").replace("\"", "") for i
+                 in star_param]
+    for curr_rating in star_list:
+        if curr_rating == 'icon-star-full':
+            movie_rating += 1
+        else:
+            movie_rating += 0.5
+    return movie_rating
+
+
+def read_html_page(home_page):
+    movie_review_dict = {}
+    result = requests.get(url=home_page)
+    result_content = result.content
+    soup_obj = BeautifulSoup(result_content, 'html5lib')
+    wrapper_class = soup_obj.find('div', id='review-list')
+    for x in wrapper_class.find_all('figure'):
+        star_list = []
+        movie_title = x.find('h5', class_='title').a.get_text()
+        movie_critic = x.find('p', class_='byline').get_text()
+        convoluted_rating = x.find('span', class_='star-rating').find_all('i')
+        movie_review_score = get_rating(movie_title, convoluted_rating)
+        print type(x.find('h5', class_='title').find_all('a'))
+
+
+read_html_page('http://www.rogerebert.com/reviews')
