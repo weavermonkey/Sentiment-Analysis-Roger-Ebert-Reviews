56e2f1b8c4771e754e55e054fa38a3e2cd07b12e
weavermonkey <vembarvarun@gmail.com>
2018-02-18 12:06:28 +0530

    Stuff Done: 1. Downloaded base HTML manually, parsed it using the existing funcions, and createda base CSV with title, reviewer, review url, and score.
    2.

--- enrich_base_csv.py
+++ enrich_base_csv.py
@@ -1,6 +1,18 @@
 from download_reviews import read_review
 import pandas as pd
+import os
 
-x = pd.read_csv('all_review_details.csv')
+base_csv = pd.read_csv('all_review_details.csv').to_dict()
+base_csv['rogerebert.com_review_text'] = {}
 
-print x.head()
+for i in range(len(base_csv['movie_title'])):
+    curr_movie_url = base_csv['ebert.com_review_url'][i]
+    print i, curr_movie_url, '\n#############################'
+    curr_file_name = os.path.normpath(
+        'C:/Users/prog/Desktop/test_github_repo/Sentiment-Analysis-Roger-Ebert-Reviews/reviews/' + str(i + 1) + '.txt')
+    if not os.path.exists(curr_file_name):
+        curr_review = read_review(url=curr_movie_url)
+        with open(curr_file_name, 'w') as file_to_write:
+            file_to_write.write(curr_review)
+# csv_with_review_df = pd.DataFrame(base_csv)
+# csv_with_review_df.to_csv('aioo.csv', index=False, encoding='utf-8')
--- review_from_url.py
+++ review_from_url.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
-from download_reviews import get_rating, read_review
+import csv
+from download_reviews import get_rating
 
-initial_url = "https://www.rogerebert.com/reviews?great_movies=0&no_stars=0&order=newest&title=te+rock&filters[great_movies][]=&filters[no_stars][]=&filters[no_stars][]=1&filters[title]=&filters[reviewers]=&filters[genres]=&page="
+initial_url = "https://www.rogerebert.com/reviews?great_movies=0&no_stars=0&order=ol&title=te+rock&filters[great_movies][]=&filters[no_stars][]=&filters[no_stars][]=1&filters[title]=&filters[reviewers]=&filters[genres]=&page="
 next_url = initial_url
 movie_data_rows = []
 i = 0
 
 
-def save_base_csv(home_page):
+def save_base_csv():
     # movie_data_rows = []
-    result_content = requests.get(home_page).content
-    soup_obj = BeautifulSoup(result_content, 'html5lib')
+    #result_content = requests.get(home_page).content
+    soup_obj = BeautifulSoup(open('ebert_complete_site.html','r'), 'html5lib')
+    i = 0
     wrapper_class = soup_obj.find('body').find('div', id='review-list')
     for curr_movie_dom in wrapper_class.find_all('figure'):
         movie_details = {'movie_title': '', 'reviewed_by': '', 'ebert.com_score': 0.0, 'ebert.com_review_url': ''}
@@ -25,12 +27,24 @@ def save_base_csv(home_page):
                            curr_movie_dom.find('h5', class_='title').find_all('a')[0]['href'][8:]
         movie_details['movie_title'] = movie_title.strip()
         movie_details['reviewed_by'] = movie_critic.strip()
-        movie_details['ebert.com_score'] = movie_review_score
+        movie_details['ebert.com_score'] = str(movie_review_score)
         movie_details['ebert.com_review_url'] = movie_review_url
-        movie_data_rows.append(movie_details)
+        i += 1
+        print i,'\n', movie_details,'\n######################################################'
+        with open('all_review_details.csv','ab') as file_to_write:
+            csv_writer = csv.writer(file_to_write)
+            csv_writer.writerow( [x.encode('utf-8') for x in movie_details.values() ] )
+        #movie_data_rows.append(movie_details
 
 
-while requests.get(next_url).status_code != '404':
-    save_base_csv(next_url)
-    i += 1
-    next_url = initial_url + str(i)
+def scroll_and_retrieve(num_pages):
+    global next_url
+    for i in range(num_pages):
+        while requests.get(next_url).status_code != '404':
+            save_base_csv(next_url)
+            i += 1
+            next_url = initial_url + str(i)
+
+#scroll_and_retrieve(num_pages=100)
+
+save_base_csv()
